e29d51047ac2357c4d263cd3a900942a711d5bdd
SainasFede <sainasfede@gmail.com>
2022-11-25 19:27:05 +0100

    add log

--- src/components/partialMain/SectionLog.vue
+++ src/components/partialMain/SectionLog.vue
@@ -6,8 +6,15 @@ export default {
 
 <template>
   <section class="log">
-    <div class="container h-100">
-      
+    <div class="container h-100 d-flex justify-content-center align-items-center">
+      <ul class="d-flex">
+        <li><img src="../../assets/img/logo-1.png" alt=""></li>
+        <li><img src="../../assets/img/logo-2.png" alt=""></li>
+        <li><img src="../../assets/img/logo-3.png" alt=""></li>
+        <li><img src="../../assets/img/logo-4.png" alt=""></li>
+        <li><img src="../../assets/img/logo-5.png" alt=""></li>
+        <li><img src="../../assets/img/logo-1.png" alt=""></li>
+      </ul>
     </div>
   </section>
 </template>
@@ -21,5 +28,14 @@ export default {
     width: 100%;
     height: 300px;
     background-color: $title1;
+    ul{
+      list-style: none;
+      li{
+        padding: 0 30px;
+        img{
+          filter: brightness(0.3) sepia(1) hue-rotate(180deg) saturate(5) grayscale(50%);
+        }
+      }
+    }
   }
 </style>
